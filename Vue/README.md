7f678f64150e815b4191a7008399b67bd053d1b3
bmkazuya <laughing00boomer@gmail.com>
2022-10-26 17:48:46 +0900

    Vueプロジェクト新規作成
    クイックスタート | Vue.js ( https://ja.vuejs.org/guide/quick-start.html ) に従っ
    てチュートリアルを進めていく。TypeScriptoとVue Routerを有効にしておけばひととおり
    の機能の確認はできると思う

---
+++ Vue/README.md
@@ -0,0 +1,29 @@
+### [クイックスタート | Vue.js](https://ja.vuejs.org/guide/quick-start.html)
+
+とりあえずTypeScriptとVue Routerを有効にしておけば機能の確認はできるかな
+
+```
+$ npm init vue@latest
+Need to install the following packages:
+  create-vue@latest
+Ok to proceed? (y)
+
+Vue.js - The Progressive JavaScript Framework
+
+✔ Project name: … vue-project
+✔ Add TypeScript? … No / Yes
+✔ Add JSX Support? … No / Yes
+✔ Add Vue Router for Single Page Application development? … No / Yes
+✔ Add Pinia for state management? … No / Yes
+✔ Add Vitest for Unit Testing? … No / Yes
+✔ Add an End-to-End Testing Solution? › No
+✔ Add ESLint for code quality? … No / Yes
+
+Scaffolding project in /home/bmkazuya/Git/botamotch/TypeScript/Vue/vue-project...
+
+Done. Now run:
+
+  cd vue-project
+  npm install
+  npm run dev
+```
---
+++ Vue/vue-project-router/src/views/AboutView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ message }}</p>
+    <button @click="increment">Count is: {{ count }}</button>
+    <p> {{ name }}  has published books : {{ publishedBooksMessage }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: "hello",
+      count: 0,
+      name: 'Jone Doe',
+      books: [
+        'Vue 2 - Advanced Guide',
+        'Vue 3 - Basic Guide',
+        'Vue 4 - The Mystery'
+      ]
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+      if (this.message == 'hello') {
+        this.message = 'goodbye'
+      } else {
+        this.message = 'hello'
+      }
+    }
+  },
+  computed: {
+    publishedBooksMessage() {
+      return this.books.length > 0 ? 'Yes' : 'No'
+    }
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
